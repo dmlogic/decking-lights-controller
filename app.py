1267ca24fd63dfd4ba3369ff38523796fb837d8c
Darren Miller <darren@darrenm.net>
2022-09-30 16:51:07 +0100

    Enable control of up-and-down lights from same app

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ pi = pigpio.pi()
 
 # Default light values
 ONOFF = ''
+POSTS = 0
 RGB = '#000000'
 DATAPATH = 'data.json'
 
@@ -17,7 +18,7 @@ def isGrey(string):
     chars = list(string.lstrip('#'))
     return (chars[0]+chars[1]) == (chars[2]+chars[3])  == (chars[4]+chars[5])
 
-def setLightValues(red, green, blue, white):
+def setLedValues(red, green, blue, white):
     # Commented for non-pi testing. Bring back to make it do stuff
     if(white):
         print ("set white to ", white)
@@ -32,6 +33,11 @@ def setLightValues(red, green, blue, white):
         # pi.set_PWM_dutycycle(25, green)
         # pi.set_PWM_dutycycle(18, 0)
 
+def controlPosts(updateValue):
+    GPIOVALUE = 0 if updateValue == 'checked' else 1
+    print ("set posts GPIO to ", GPIOVALUE)
+    # pi.write(16, GPIOVALUE)
+
 def persistStatus(updateData):
     with open(DATAPATH, 'w') as f:
         json.dump(updateData, f)
@@ -42,36 +48,43 @@ def index():
 
 @app.route('/update', methods=['POST'])
 def update():
-    global RGB, ONOFF
+    global RGB, ONOFF, POSTS
 
-    # Turn it all off
+    # Deal with posts circuit first
+    POSTS = request.form.get('onoffPosts')
+    controlPosts(POSTS)
+
+    # Turn LED circuit off
     if request.form.get('onoff') == 'off':
         ONOFF=''
-        setLightValues(0, 0, 0, 0)
+        setLedValues(0, 0, 0, 0)
 
-    # Set on values as per data supplied
+    # Set LED values as per data supplied
     else:
         ONOFF='checked'
         RGB = request.form.get('rgb')
         rgbValues = tuple(int(RGB.lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
         if(isGrey(RGB)):
-            setLightValues(0, 0, 0, rgbValues[0])
+            setLedValues(0, 0, 0, rgbValues[0])
         else:
-            setLightValues(rgbValues[0], rgbValues[1], rgbValues[2], 0)
+            setLedValues(rgbValues[0], rgbValues[1], rgbValues[2], 0)
 
     # Store data for persistence
-    updateData = {"onoff": ONOFF, "rgb": RGB}
+    updateData = {"onoff": ONOFF, "rgb": RGB, "posts": POSTS}
     persistStatus(updateData)
     return updateData
 
 @app.route('/status', methods=['GET'])
 def status():
+    fallbackData = {"onoff": ONOFF, "rgb": RGB, "posts": POSTS, "error": "no data"}
     if exists(DATAPATH):
         with open(DATAPATH, 'r') as f:
             try:
                 return json.load(f);
             except:
-                return {"onoff": ONOFF, "rgb": RGB, "error": "no data"}
+                return fallbackData
+
+    return fallbackData
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
